cdbc8cd01a40e943bbde1ca9bf141a61342911cf
pavel <prshir@yandex.ru>
2022-02-13 15:01:32 +0500

    задача 27

---
+++ 27. Remove Element.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        place_ind = len(nums)-1
+        for i_ in range(len(nums),0,-1):
+            i = i_-1
+            print(i)
+            if nums[i] == val:
+                nums[place_ind], nums[i] = nums[i], nums[place_ind]
+                place_ind -= 1
+        print(nums)
+        return len(nums)-nums.count(val)
+
+solution = Solution()
+print(solution.removeElement([0,1,2,2,3,0,4,2], 2))
